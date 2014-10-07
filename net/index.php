c629eee6247fe02bb66ad9970cde3ba34505d857
Elliot <ere16@case.edu>
2014-10-06 23:15:44 -0400

    started net project

---
+++ net/index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<html>
+    <head>
+        <style>
+            body{
+                background-color: grey;
+                overflow: scroll;
+            }
+        </style>
+    </head>
+    <body>
+        body here
+    </body>
+    <script>
+        function log(str) {
+            document.write("</br>"+str);
+        }
+        log("loaded");
+    </script>
+</html>
