ff964e2a943532ad75a8198556a515c7c80df92f
Elliot <ere16@case.edu>
2014-11-20 15:58:59 -0500

    more fixes to matchmaking

--- cardGame/index.php
+++ cardGame/index.php
@@ -6,22 +6,52 @@ redirectIfLoggedOut();
 <html>
     <head>
         <script src="util.js"></script>
-        <script>
+    </head>
+    <body>
+        <input id='matchBtn' type="button" onclick="search=true,match()" value="Enter match"></input>
+        <input id='stopMatchBtn' type="button" onclick="stopMatch()" value="Stop matching"></input>
+        <a href="logout.php">Logout</a>
+        <div id="msg"></div>
+    </body>
+    <script>
+            var tries = 0;
+            var msgBox = document.getElementById('msg');
+            var matchButton = document.getElementById('matchBtn');
+            var stopMatchButton = document.getElementById('stopMatchBtn');
+            var search = false;
             function match(){
+                //search = true; [in matchBtn]
+                matchButton.style.display='none';
+                stopMatchButton.style.display='block';
                 sendRequest("matchmaker.php",
                             "",
                             function(response){
-                                alert(response);
-                                /*var json = JSON.parse(response);
-                                for(i in json){//user, ip
-                                    alert(json[i].msg);
-                                }*/
+                                var found = false;
+                                var json = JSON.parse(response);
+                                for(i in json){
+                                    if(typeof json[i].opp != 'undefined'){
+                                        //if opponent found
+                                        msgBox.innerHTML+="<a href='arena.php?o="+json[i].opp+"'>Battle</a>";
+                                        stopMatchButton.style.display='none';
+                                        matchButton.style.display='none';
+                                        found = true;
+                                    }
+                                }
+                                if (!found) {
+                                    tries++;
+                                    msgBox.innerHTML = "matchmaking attempts: "+tries;
+                                    if (search) {
+                                        setTimeout(match(), 5000);
+                                    }
+                                }
                             });
             }
+            
+            function stopMatch(){
+                search = false;
+                tries = 0;
+                stopMatchButton.style.display='none';
+                matchButton.style.display='block';
+            }
         </script>
-    </head>
-    <body>
-        <input type="button" onclick="match()" value="Enter match"></input>
-        <a href="logout.php">Logout</a>
-    </body>
 </html>
